698c0b8f1c0dd5b19df321ff2a2bc99d1778460d
Nick Freear <nfreear@yahoo.co.uk>
2012-08-09 16:04:01 +0100

    Adding the Layout library, and layout_ci site-layout view

---
+++ application/libraries/Layout.php
@@ -0,0 +1,42 @@
+<?php  
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Layout
+{
+    
+    var $obj;
+    var $layout;
+    
+    function Layout($layout = "layout_main")
+    {
+#ou-specific bug fix
+        if (is_array($layout)) {
+            $layout = isset($layout['layout']) ? $layout['layout'] : $layout[0];
+        }
+#ou-specific ends.
+        $this->obj =& get_instance();
+        $this->layout = $layout;
+    }
+
+    function setLayout($layout)
+    {
+      $this->layout = $layout;
+    }
+    
+    function view($view, $data=null, $return=false)
+    {
+        $loadedData = array();
+        $loadedData['content_for_layout'] = $this->obj->load->view($view,$data,true);
+        
+        if($return)
+        {
+            $output = $this->obj->load->view($this->layout, $loadedData, true);
+            return $output;
+        }
+        else
+        {
+            $this->obj->load->view($this->layout, $loadedData, false);
+        }
+    }
+}
+?>

ad70a6d9643e2688f5b4ca3a5962e0d8d3bb0e04
Nick Freear <nfreear@yahoo.co.uk>
2012-08-09 16:54:15 +0100

    Numerous small fixes to CSS, change to relative font-size and so on

--- application/views/site_layout/layout_ci.php
+++ application/views/site_layout/layout_ci.php
@@ -3,7 +3,12 @@
 ?>
 <!doctype html><html lang="en"><meta charset="utf-8" /><title>Track OER</title>
 
+<!--[if lt IE 9]>
+<link rel="stylesheet" href="http://www3.open.ac.uk/includes/ouice/3/ie.css" />
+<![endif]-->
+
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="ROBOTS" content="noindex,nofollow" />
 
 <link rel=stylesheet href="<?php echo $assets_url ?>layout-ci.css" />
 <link rel=stylesheet href="<?php echo $assets_url ?>forkme.css" />
@@ -15,12 +20,12 @@
 
 
 	<?php echo $content_for_layout ?>
-	
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+
+	<p class="ci-footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
-<div id="banner"><a href="https://github.com/IET-OU/trackoer-core">Fork me on GitHub</a></div>
+<div id="forkme-banner"><a href="https://github.com/IET-OU/trackoer-core" title="Fork me on GitHub">Fork me on GitHub</a></div>
 <?php /*
 <a href="http://github.com/you"><img style="position: absolute; top: 0; left: 149px; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub"></a>
 */ ?>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,29 +1,30 @@
 
-<?php /*<article class="markdown-body entry-content" itemprop="mainContentOfPage">*/ ?>
+<article class="markdown-body entry-content" itemprop="mainContentOfPage">
 
 <h1><a name="trackoer-core" class="anchor" href="#trackoer-core"><span class="mini-icon mini-icon-link"></span></a>Track OER</h1>
 
-<p><strong>Track OER</strong> is a JISC and HEFCE-funded project to facilitate Web analytics for Open Educational Resources.</p>
+<p><strong>Track OER</strong> is a <abbr title="Joint Information Systems Committee">JISC</abbr> and
+<abbr title="Higher Education Funding Council for England">HEFCE</abbr>-funded project to facilitate Web analytics for Open Educational Resources.</p>
 
 <p><a href="https://github.com/IET-OU/trackoer-core">trackoer-core</a> is the central web application for the Project, providing clean URL
-redirection to no-Javascript web-bugs for Piwik (and Google Analytics), and test/demonstration pages.</p>
+redirection to no-Javascript web-bugs for <a href="http://piwik.org/">Piwik</a> (and Google Analytics), and test/demonstration pages.</p>
 
 <h2><a name="links" class="anchor" href="#links"><span class="mini-icon mini-icon-link"></span></a>Links</h2>
 
 <ul>
-<li>Project blog: <a href="http://cloudworks.ac.uk/tag/view/trackoer">cloudworks.ac.uk/tag/view/trackoer</a>
-<li>piwik-trackoer:  <a href="https://github.com/IET-OU/piwik-trackoer">github.com/IET-OU/piwik-trackoer</a>
-<li>JISC page: <a href="http://jisc.ac.uk/whatwedo/programmes/ukoer3/rapidinnovation/trackoer.aspx">jisc.ac.uk/whatwedo/programmes/ukoer3/rapidinnovation/trackoer.aspx</a>
+<li>Project blog: <a href="http://cloudworks.ac.uk/tag/view/trackoer">Cloudworks/ tag/ view/ trackoer</a>
+<li>piwik-trackoer:  <a href="https://github.com/IET-OU/piwik-trackoer">Github/ IET-OU/ piwik-trackoer</a>
+<li>JISC page: <a href="http://jisc.ac.uk/whatwedo/programmes/ukoer3/rapidinnovation/trackoer.aspx">JISC/ whatwedo/ programmes/ ukoer3/ rapidinnovation/ trackoer</a>
 </ul>
 
 <h2><a name="credits" class="anchor" href="#credits"><span class="mini-icon mini-icon-link"></span></a>Credits</h2>
 
-<p>trackoer-core: Copyright 2012 The Open University.</p>
+<p>trackoer-core: <a href="http://www.open.ac.uk/copyright">Copyright 2012 The Open University</a>.</p>
 
 <ul>
-<li>License:  [free/open source -- license to be decided]
-<li>Authors:   Institute of Educational Technology, and others.
+<li>License: [free/open source -- license to be decided]
+<li>Authors: <a href="http://iet.open.ac.uk/">Institute of Educational Technology</a> | <a href="http://twitter.com/nfreear">@nfreear</a>, and others.
 </ul>
 
-<?php /*</article>*/ ?>
+</article>
 

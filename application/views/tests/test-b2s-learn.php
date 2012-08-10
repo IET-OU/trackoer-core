b3c2410dcb89f33dc31889328c983ae1d28c6198
Nick Freear <nfreear@yahoo.co.uk>
2012-08-10 16:24:52 +0100

    Adding more content, user-warning etc. to B2S Learning to Learn test view

--- application/views/tests/test-b2s-learn.php
+++ application/views/tests/test-b2s-learn.php
@@ -1,6 +1,54 @@
 
-<h2>Creative Commons license tracker</h2>
+<div class=warn><p>Note, this is a test/ demonstration page, which contains a Creative Commons license <a href="#cc-code">image-tracker</a>.</div>
 
-<p id=cc-code>
+<div class="test-content ou b2s">
+
+<h2>Learning to Learn (Learning_to_Learn_1.0)</h2>
+
+
+<p class="section img-text">
+<p class="activity label label_text  figure"><!--<div class="centered"><div class="inline_relpos">--><img src="http://labspace.open.ac.uk/file.php/7442/y165_1_front.jpg" alt=""/></li>
+<p class="activity label label_text">Welcome to Learning to Learn! By accessing this course you have just taken a major
+        step toward helping you develop your study skills. Congratulations! Your first task is to
+        read the course overview and complete the self-reflection questionnaire. This will help you
+        to think about how Learning to Learn can help you achieve your goals and aims.</li>
+<p class="activity label label_text"><b>Time</b>: 100 hours<br /><b>Level</b>: Introductory
+
+<br /><b>Contributor</b>: <a href="http://labspace.open.ac.uk/user/view.php?id=178688&course=1">Andrew Studnicky</a>
+</li>
+<p class="activity questionnaire"><a   href="http://labspace.open.ac.uk/mod/questionnaire/view.php?id=460392&amp;direct=1"><img src="http://labspace.open.ac.uk/pix/smartpix.php/oci_labspace/mod/questionnaire/icon.gif" class="activityicon" alt="" /> <span>Self-Reflection Questionnaire<span class="accesshide " >  </span></span></a></li>
+<p class="activity label label_text"><h2>Unit 1: Course Overview</h2>
+
+<div class="oucontent-content"><h1 class="oucontent-title-1">Introduction</h1><p><i>Learning to Learn</i> helps you think about what you can do already. It
+then uses this to build your confidence in your abilities. The course uses a mixture
+of personal reflection, examples (including three real-life case studies), and ideas
+about how we learn. This combination equips you to move your life
+forward.</p><p>The course is divided into units, and each includes information and
+activities to help you achieve specific goals, known as learning
+outcomes.</p><p>By the time you have reached the end of Unit 1: Course Overview, you should
+be able to...</p><ul class="oucontent-bulleted"><li>...understand <b>what the course is about</b> and how it is
+structured.</li><li>...appreciate <b>what will be required</b> of you as a student in the
+course.</li><li>...understand <b>the importance of the word <i>skills</i></b>.</li><li>...start thinking about <b>your own learning</b>.</li></ul><p>By the time you have reached the end of Unit 2: You and Your Learning, you
+should be able to...</p><ul class="oucontent-bulleted"><li>...understand that valuable and important learning goes on <b>all the
+time</b>.</li><li>...appreciate that <b>learning can involve thinking, doing and
+feeling</b>.</li><li>...develop a clearer idea of <b>what you have learned</b> and what
+qualities, knowledge, and skills you already have.</li><li>...better understand <b>the importance of <i>everyday</i> skills</b> of
+communication, problem solving, and organization.</li></ul><p>By the time you have reached the end of Unit 3: Exploring Learning, you
+should be able to...</p><ul class="oucontent-bulleted"><li>...use <b>feedback to add information</b> about your qualities, knowledge,
+and skills from another perspective.</li><li>...use academic approaches to learning to <b>increase your
+understanding</b> of your qualities, knowledge, and skills.</li><li>...reflect on <b>the academic skills</b> you have used.</li></ul><p>By the time you have reached the end of Unit 4: Where Next? you should be
+able to...</p><ul class="oucontent-bulleted"><li>...explain how the wider social and political context <b>may affect your
+learning</b>.</li><li>...<b>choose at least one goal</b> that you would like to work toward and
+explain why you have chosen that goal.</li><li>...identify <b>what you can do</b> to help you achieve your
+goal.</li><li>draw up a plan for achieving your goal.</li><li>...explain how you have used any ideas presented in the unit to
+<b>identify your goals</b>.</li></ul></div>
+
+
+<p class=elip> . . . </p>
+
+
+</div>
+<div id=cc-code><p>
 <?php echo $cc_code ?>
+</div>
 

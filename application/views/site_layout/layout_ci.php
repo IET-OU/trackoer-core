484d42294fa970efdc87e2d435a5885ed98edf71
Nick Freear <nfreear@yahoo.co.uk>
2012-08-10 16:26:11 +0100

    Adding CSS for user-warning etc. to layout-ci stylesheet

--- application/views/site_layout/layout_ci.php
+++ application/views/site_layout/layout_ci.php
@@ -56,15 +56,16 @@
 		<li class="f-tx"><a href="http://www.open.ac.uk/conditions">Conditions of use</a>
 		<li class="f-tx"><a href="#">Contact us/ Feedback</a>
 		<li class="ci-footer">Page rendered in <strong>{elapsed_time}</strong> seconds</li>
+		<li id="forkme-banner"><a href="https://github.com/IET-OU/trackoer-core" title="Fork me on GitHub">Fork me on GitHub</a></li>
 		</ul>
 	</div>
 <?php else: ?>
 	<p class="ci-footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	<div id="forkme-banner"><a href="https://github.com/IET-OU/trackoer-core" title="Fork me on GitHub">Fork me on GitHub</a></div>
 <?php endif; ?>
 
 </div>
 
-<div id="forkme-banner"><a href="https://github.com/IET-OU/trackoer-core" title="Fork me on GitHub">Fork me on GitHub</a></div>
 <?php /*
 <a href="http://github.com/you"><img style="position: absolute; top: 0; left: 149px; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub"></a>
 */ ?>

d088bc804ecf95788ba1c0d072e1ca0e51fde868
Nick Freear <nfreear@yahoo.co.uk>
2012-08-15 17:55:54 +0100

    Big rework of test-ga-js-learning1 page/ view - to use updated Javascript

--- application/views/tests/test-ga-js-learning1.php
+++ application/views/tests/test-ga-js-learning1.php
@@ -1,14 +1,10 @@
 <?php
-  $public_js_url = base_url() .'/application/assets/public/js/';
-  $google_analytics_id = 'UA-34064304-2';
 
+  $public_js_url = base_url() .'public/js/';
+  $google_analytics_id = 'UA-34064304-2';
+  $piwik_siteid = 4;
 
 ?>
-
-<?php /*<script src="https://raw.github.com/tatemae/capret/master/public/js/oer_license_parser.js"></script>*/ ?>
-<script src="<?php echo $public_js_url ?>oer_license_parser.js"></script>
-<?php /*<script src="<?php echo $public_js_url ?>parseuri.js"></script>*/ ?>
-<script src="<?php echo $public_js_url ?>parseurl-dom.js"></script>
 
 <script>
   var _gaq = _gaq || [];
@@ -23,14 +19,16 @@
 </script>
 
 
+
 <h1>Track OER - Google Analytics custom Javascript</h1>
 
 
-<p>[Creative commons license snippet]
+<p>[Creative Commons RDFa license snippet]
+
 <p>
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/deed.en_GB"
  ><img alt="Creative Commons Licence" style="border-width:0" src=
- "http://track.olnet.org/track/r/piwik/4/cc:by-nc-sa/labspace.open.ac.uk/Learning_to_Learn_1.0?p=mod%2Foucontent%2Fview.php%3Fid%3D471422%26section%3D3&t=2.3+Gathering+Evidence%E2%80%94Your+Qualities%2C+Knowledge+and+Skills+-+Learning+to+Learn+-+LabSpace+%28Course%29&debug=2"
+ "http://track.olnet.org/track/r/piwik/<?php echo $piwik_siteid ?>/cc:by-nc-sa/labspace.open.ac.uk/Learning_to_Learn_1.0?p=mod%2Foucontent%2Fview.php%3Fid%3D471422%26section%3D3&t=2.3+Gathering+Evidence%E2%80%94Your+Qualities%2C+Knowledge+and+Skills+-+Learning+to+Learn+-+LabSpace+%28Course%29&debug=2"
  class="wb" title="Creative Commons License - with tracking**" 
  /></a>
  <br />
@@ -41,47 +39,20 @@
  <br />
  Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://labspace.open.ac.uk/mod/oucontent/view.php?id=471422&amp;section=3#frag" rel="dct:source"
  >http://labspace.open.ac.uk/mod/oucontent/view.php?id=471422&amp;section=3</a>.
-
+<!--Extend Creative Commons with a course 'identifier'	-->
  <br />
  Identifier: <a xmlns:dct="http://purl.org/dc/terms/" href="http://labspace.open.ac.uk/Learning_to_Learn_1.0" rel="dct:identifier">Learning_to_Learn_1.0</a>
 </p>
 
 
-<p>[custom javascripts . . . <br /> . . . _gaq.push(['_trackPageview', path]); . . . ]
 
-
+<script src="<?php echo $public_js_url ?>oer_license_parser.js"></script>
+<?php /*<script src="<?php echo $public_js_url ?>parseuri.js"></script>*/ ?>
+<script src="<?php echo $public_js_url ?>parseurl-dom.js"></script>
+<script src="<?php echo $public_js_url ?>trackoer-page-url.js"></script>
 <script>
-/*(c)2012-08-15 The Open University.
-*/
-(function trackoer(){
-  var
-    debug = true
-  , Uri = parseURL //Was: parseUri() -- but we need 'segments'!
-  , Encode = encodeURIComponent
-  , M = Math
-  , Log = function(ob){if(typeof console!=='undefined'){console.log(arguments)}}
-  , license = oer_license_parser.get_license()
-  , current = license.current_license
-  , source_url = Uri(current.source_url) // Page-level Actual course http://labpspace.open.ac.uk/mod/oucontent/view.php?id=1234&section=5
-  , source_id = Uri(current.source_id)   // Course-level, Eg. http://labspace.open.ac.uk/Learning_to_Learn_1.0
-  , dl = document.location
-  , sp = '!'
-  , path = Uri(dl).relative
-  ;
-
-  path += (path.indexOf('#') == -1 ? '#' :'');
-  path += sp + Encode(source_url.host) + sp + Encode(source_id.segments[0]) + sp + Encode(source_url.relative);
-
-  path += debug ? sp + 'Debug' + sp + M.floor((M.random()*100)+1) :'';
-
-
-// To move!
-  _gaq.push(['_trackPageview', path]);
-
-
-  //Log(oer_license_parser);
-  Log(current, source_url, path);
-})();
+  // Just the one line -- after the RDFa!
+  _gaq.push(['_trackPageview', trackoer.getPageUrl()]);
 </script>
 
 

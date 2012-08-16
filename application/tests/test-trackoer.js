e6d150503e2b422e5e804ea385ac28d4219e827d
Nick Freear <nfreear@yahoo.co.uk>
2012-08-16 12:00:20 +0100

    Busterjs unit test Javascript - remove excess comments, whitespace

--- application/tests/test-trackoer.js
+++ application/tests/test-trackoer.js
@@ -9,8 +9,6 @@
 */
 
 
-//setTimeout(function() {
-
 if (typeof require == "function" && typeof module == "object") {
     buster = require("buster");
     //require("./strftime");
@@ -19,32 +17,29 @@ if (typeof require == "function" && typeof module == "object") {
 var assert = buster.assert;
 
 buster.testCase("buster.js: Custom Google Analytics tests", {
-    setUp: function () {
+	setUp: function () {
 		//this.date = new Date(2009, 11, 5);
 
 		//console.log($("a[rel='dct:source']").attr('href'));
-		//console.log($("img[src*='/track/r/piwik/']").attr('src'));
-
-		//console.log($("img[src*='__utm.gif']").attr('src'));
-		//console.log(window._gat, window._gat._getTracker);
-		//console.log(window._gat._getTrackerByName()._getAccount());
-    }
+		//console.log($("img[src*='__utm.gif']").attr('src')); //setTimeout doesn't help...
+	}
 
 
-    , "DOM: document": function() {
-		assert.isObject(document);
+	, "DOM: document": function() {
+		assert.isObject( document );
 	}
 
 // Google Analytics.
 	// https://developers.google.com/analytics/resources/articles/gaTrackingTroubleshooting#gaDebug
 	// http://stackoverflow.com/questions/1954910/javascript-detect-if-google-analytics-is-loaded-yet
+
 	, "Google Analytics: _gat._getTracker() exists": function() {
 		//NO! assert.isString($("img[src*='__utm.gif']").attr('src'));
-		assert.isFunction(window._gat._getTracker);
+		assert.isFunction( window._gat._getTracker );
 	}
 
 	, "Google Analytics: _gat..._getAccount() string": function() {
-		assert.isString(window._gat._getTrackerByName()._getAccount());
+		assert.isString( window._gat._getTrackerByName()._getAccount() );
 		//Fail - why? assert.equals(window._gat._getTrackerByName()._getAccount(), /UA-\d+-\d+/);  /* /UA-\d{4,10}-\d{1,2}/); */
 	}
 
@@ -55,25 +50,26 @@ buster.testCase("buster.js: Custom Google Analytics tests", {
 
 // RDFa.
 	, "RDFa: rel=license": function() {
-	    assert.isString($("a[rel='license']").attr('href'));
+	    assert.isString( $("a[rel='license']").attr('href') );
 	}
 
 	, "RDFa: rel=dct:source": function() {
-	    assert.isString($("a[rel='dct:source']").attr('href'));
+	    assert.isString( $("a[rel='dct:source']").attr('href') );
 	}
 
 	, "RDFa: rel=dct:identifier": function() {
-	    assert.isString($("a[rel='dct:identifier']").attr('href'));
+	    assert.isString( $("a[rel='dct:identifier']").attr('href') );
 	}
 
 // trackoer-page-url.js
 	, "trackoer-page-url.js: trackoer.getPageUrl() exists": function() {
-	    assert.isFunction(trackoer.getPageUrl);
+	    assert.isFunction( trackoer.getPageUrl );
 	}
 
 	, "trackoer-page-url.js: trackoer.getPageUrl() string": function() {
-	    assert.isString(trackoer.getPageUrl());
+	    assert.isString( trackoer.getPageUrl() );
 	}
-});
 
-//} , 1000);
+//...?
+
+});

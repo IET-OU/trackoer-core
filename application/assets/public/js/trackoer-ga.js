412ae74794624326f234241c67afed6a8bced7c4
Nick Freear <nfreear@yahoo.co.uk>
2012-09-05 12:37:21 +0100

    JSLint fixes for trackoer-ga Javascript/JS

--- application/assets/public/js/trackoer-ga.js
+++ application/assets/public/js/trackoer-ga.js
@@ -2,38 +2,40 @@
  trackoer.getPageUrl version 2 + Google Analytics asynch.
  (c)2012-08-20 The Open University.
 */
+/*jslint browser: true, devel: true, indent: 2 */
+
 var trackoer = trackoer || {};
 
-trackoer.getPageUrl = function(custom_hash) {
-  var
-    debug = true
-  , Enc = encodeURIComponent
-  , M = Math
-  , Log = function(ob){if(typeof console!=='undefined'){console.log(arguments)}}
-  , Sec = function(u){return u.replace(/(C:\/Users\/|C:\/)/g, '').replace(/file:\/\//, 'http://F')}
-  , dl = document.location
-  , sp = '!'
-  , path = /*dl.hostname +*/ Sec(dl.pathname) + Enc(dl.search + dl.hash)
-  ;
+trackoer.getPageUrl = function (custom_hash) {
+  'use strict';
 
-  path += (path.indexOf('#') == -1 || path.indexOf('%23') == -1 ? '%23' :'');
-  path += Enc(custom_hash.replace(/&amp;/g, '&'));
-  path += sp + Enc(dl.protocol); //.replace(/:/, ',')
+  var debug = true,
+    enc = encodeURIComponent,
+    M = Math,
+    log = function (ob) {if (typeof console !== 'undefined') {console.log(arguments); } },
+    sec = function (u) {return u.replace(/(C:\/Users\/|C:\/)/g, '').replace(/file:\/\//, 'http://F'); },
+    DL = document.location,
+    sp = '!',
+    path = /*DL.hostname +*/ sec(DL.pathname) + enc(DL.search + DL.hash);
 
-  path += debug ? sp + 'Debug' + sp + M.floor((M.random()*100)+1) :'';
-  Log(path);
-  Log(dl);
+  path += (path.indexOf('#') === -1 || path.indexOf('%23') === -1 ? '%23' : '');
+  path += enc(custom_hash.replace(/&amp;/g, '&'));
+  path += sp + enc(DL.protocol); //.replace(/:/, ',')
 
-  //return Enc(path);
+  path += debug ? sp + 'Debug' + sp + M.floor((M.random() * 100) + 1) : '';
+  log(path);
+  log(DL);
+
+  //return enc(path);
   return path;
 };
 
 
 var _gaq = _gaq || [];
 /*ga-start*/
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+(function () {
+  var s, D = document, ga = D.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' === D.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  s = D.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 /*ga-end*/

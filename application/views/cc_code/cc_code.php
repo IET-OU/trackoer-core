0658a7fb2bac40fb9b93a65dfeaf0ffba67bdc7e
Nick Freear <nfreear@yahoo.co.uk>
2012-09-05 23:48:26 +0100

    Wiring localized/ i18n 'cc_label' into the Creative Commons cc_code view

--- application/views/cc_code/cc_code.php
+++ application/views/cc_code/cc_code.php
@@ -15,10 +15,12 @@
  /></a>
  <br />
  <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"><?php echo $title ?></span>
- by <a xmlns:cc="http://creativecommons.org/ns#" href="<?php echo $author_url ?>" property="cc:attributionName" rel="cc:attributionURL"><?php echo $author ?></a>
+ by <a xmlns:cc="http://creativecommons.org/ns#" href="<?php echo $author_url ?>" property="cc:attributionName" rel="cc:attributionURL"><?php echo $author ?></a><?php echo $cc_label ?>
+<?php /*
  is licensed under a
- <a rel="license" href="http://creativecommons.org/licenses/<?php echo $cc_license ?>/deed.<?php echo $cc_loc ?>">Creative Commons Attribution-NonCommercial-ShareAlike 2.0 UK: England &amp; Wales License</a><?php
-  if($with_tracker): ?>, <a class="wt" href="<?php echo $explain_tracking_url ?>">with tracking</a><?php endif; ?>.
+ <a rel="license" href="http://creativecommons.org/licenses/<?php echo $cc_license ?>/deed.<?php echo $cc_loc ?>">Creative Commons Attribution-NonCommercial-ShareAlike 2.0 UK: England &amp; Wales License</a>
+*/ ?><?php
+  if($explain_tracking_url): ?>, <a class="wt" href="<?php echo $explain_tracking_url ?>">with tracking</a><?php endif; ?>.
  <br />
  Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="<?php echo $source_url ?>" rel="dct:source"><?php echo $source_url ?></a>.
  <!--Extend Creative Commons with a course 'identifier'-->

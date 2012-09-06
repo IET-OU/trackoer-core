8a5df1d195dc669aed476cafdc66b0f72bc3bd85
Nick Freear <nfreear@yahoo.co.uk>
2012-09-06 17:22:49 +0100

    Adding Piwik_Tracker::getBeaconUrl (redirect) & ::getBeaconUrl_V0 (direct)

--- application/libraries/trackers/Piwik_Tracker.php
+++ application/libraries/trackers/Piwik_Tracker.php
@@ -67,6 +67,67 @@ class Piwik_Tracker extends Redirect_Tracker {
     return $result;
   }
 
+
+  /**
+  * Get the URL with tracking parameters for the web-beacon/web bug.
+  * @return string URL
+  */
+  public function getBeaconUrl($site_id = NULL, $image, $source_host = NULL, $source_id, $source_path = NULL, $referer = NULL, $title = NULL) {
+
+    $params = array(
+	  'p' => $source_path, #Todo: parse
+	  't' => $title,
+    );
+    $redirect_beacon_url = site_url('track/r/piwik/'. $site_id .'/'. $image .'/'. $source_host .'/'. $source_id) .'?'. http_build_query($params);
+
+    return $redirect_beacon_url;
+  }
+
+  public function getBeaconUrl_V0($site_id = NULL, $image, $source_host = NULL, $source_path = NULL, $referer = NULL, $title = NULL, $record = 1) {
+
+	// See, http://piwik.org/docs/tracking-api/reference
+	$params = (object) array(
+	  // An integer: 1, 2..
+	  'idsite' => $site_id,
+	  'rec' => $record,
+	  // 'AlternateImage' plugin for Piwik uses 'img'.
+	  'img' => $image,
+	  // For the no-Javascript web-bug image, the OER's destination URL is the referrer.
+	  #'url' => $referer .'#!'. $source_identifier .'!'. $source_path,
+	  // The custom Page title..
+	  'action_name' => $title,
+	  // We don't have a user '_id'...
+
+	  // A random number, to avoid the tracking request being cached by the browser or a proxy.
+	  'rand' => rand(101, 999),
+	  // Piwik API version.
+	  'apiv' => 1,
+	  // And we put the OER's source URL in the 'referer' slot.
+	  'urlref' => 'http://'. $source_host .'/'. $source_path,
+	  // Visit-scope custom variables - JSON encoded.
+	  // {"0":["KEY","VALUE"]}
+	  // {"0":["license","cc:by-nc-sa/2.0/uk"]}
+	  '_cvar' => (object) array(
+	    #'0' => array('Via', __CLASS__),
+		'0' => array('Image', $image),
+	  ),
+	);
+	if ($this->CI->_is_debug(1)) {
+	  $params->url .= '!Debug!'. $params->rand;
+	  #$params->action_name .= '!Debug!'. $params->rand;
+
+	  $params->_cvar->{'1'} = array('Via', site_url('track/'. 'Piwik') .'/..');
+	  #$request->_cvar->{'2'} = array('Debug', $request->rand);
+	}
+	$this->CI->_debug($params);
+
+	$params->_cvar = json_encode($params->_cvar);
+
+	$beacon_url = $this->tracker_url . http_build_query($params);
+
+    return $beacon_url;
+  }
+
   /**
   * Implementation of track() - used by Track controller.
   * @return void

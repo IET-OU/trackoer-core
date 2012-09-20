bdb41983a74b4da9ca131b39fc388ea2c2f60f15
Nick Freear <nfreear@yahoo.co.uk>
2012-09-20 16:38:17 +0100

    Adding Layout_Markdown library, and config/markdown_references (view)

---
+++ application/config/markdown_references.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+|--------------------------------------------------------------------------
+| References for Markdown reference-style labels.
+|--------------------------------------------------------------------------
+| @link http://michelf.ca/projects/php-markdown/dingus/
+|
+| Note, this file is a cross between a config file and a view!
+*/
+?>
+
+[ga]: http://google.com/analytics
+[piwik]: http://piwik.org/
+[capret]: http://capret.mitoeit.org/ "Cut and Paste Reuse Tracking"
+[cc]: http://creativecommons.org/
+[cc-api]: http://api.creativecommons.org/ "Creative Commons API"
+[oembed]: http://oembed.com/
+
+[blog]: <?php echo BLOG_URL ?> "Track OER blog, on Cloudworks"
+[code]: <?php echo CODE_URL ?> "Track OER code, on Github"
+[jisc]: http://www.jisc.ac.uk/ "Joint Information Systems Committee"
+[hefce]: http://www.hefce.ac.uk/ "Higher Education Funding Council for England"
+[ou]: http://www.open.ac.uk/ "The Open University"
+[iet]: http://iet.open.ac.uk/ "Institute of Educational Technology"

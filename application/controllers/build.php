a5fbd2a51b33cdc8a9ed1d920c12328985c8094a
Nick Freear <nfreear@yahoo.co.uk>
2012-09-20 12:10:55 +0100

    Adding Build controller, to generate CaPRet Javascript build scripts,
    * Based on OU Player,
    * https://github.com/IET-OU/ouplayer/blob/master/application/controllers/build.php
    * Uses Closure Compiler, http://closure-compiler.appspot.com/home
    * Usage: http://track.olnet.org/build/capret/[whitespace/][1.8.0]

---
+++ application/controllers/build.php
@@ -0,0 +1,171 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Track OER
+ *
+ * A web application to facilitate analytics for Open Educational Resources.
+ *
+ * @package		trackoer-core
+ * @copyright	Copyright 2012 The Open University.
+ * @author		N.D.Freear, 17 April-20 September 2012.
+ * @license
+ * @filesource
+ */
+
+
+/**
+* Track OER Javascript builder.
+*
+* (Based on: OU player Javascript/ CSS builder.)
+*
+* In Web mode creates a Closure Compiler build script.
+* (In cli mode builds using YUI Compressor.)
+*
+*     Usage: $ \xampp\php\php index.php build/revision
+*
+* See mediaelement/src/Builder.py
+* See oup-mep/src/build.php
+*
+* @link http://closure-compiler.appspot.com/home
+* (http://www.minifyjs.com/javascript-compressor/)
+* (http://www.lotterypost.com/css-compress.aspx)
+*
+* @copyright 2012 The Open University.
+* @author N.D.Freear, 17 April 2012, -04-25.
+*/
+
+class Build extends MY_Controller {
+
+  protected $_closure_template = <<<EOF
+<h2><a href="http://track.olnet.org/capret/build/__OUTPUT__">__OUTPUT__</a></h2>
+<pre>
+// ==ClosureCompiler==
+// @output_file_name __OUTPUT__
+// @compilation_level __LEVEL__
+//__URLS__// ==/ClosureCompiler==
+
+</pre>
+
+EOF;
+
+
+  /** Build a 'revision' file (CLI).
+  */
+  public function revision() {
+    if ($this->input->is_cli_request()) {
+      $this->load->library('Gitlib', null, 'git');
+
+      $result = $this->git->put_revision();
+
+	} else {
+	  $this->_error('The page you requested was not found.', 404);
+	}
+  }
+
+
+  /** Build a theme (Web/CLI).
+  */
+  public function capret($optimizations = 'whitespace', $jquery_version = NULL, $capret_mode = NULL, $output = '') {	
+    #$optimizations = strtoupper($optimizations);
+
+    #$this->load->theme($theme_name);
+    $this->load->tracker('Capret');
+
+    if ($this->input->is_cli_request()) {
+      $this->load->library('Gitlib', null, 'git');
+      $result = $this->git->put_revision();
+
+      $this->_cli_builder();
+    } else {
+      $this->_web_closure($optimizations, $jquery_version);
+    }
+  }
+
+
+  /** Print all theme Closure scripts in a Web page.
+  */
+  protected function _web_closure($optimizations, $jquery_version = NULL) {
+
+    $capret_modes = array('classic', 'piwik', 'ga');
+?>
+<!doctype html><title>*Closure compiler script | Track OER</title>
+<a href="http://closure-compiler.appspot.com/home">closure-compiler.appspot.com/home</a>
+
+<?php foreach ($capret_modes as $mode): ?>
+<p id=<?php echo $mode ?> >CaPReT mode: <?php echo $mode ?>
+
+<?php
+    $scripts = $this->tracker->getJavascripts($mode, $jquery_version);
+    $js_min = str_replace('-classic', '', 'capret-'. $mode) .'.min.js';
+
+    // Build script for minified Javascripts.
+    echo $this->_closure($scripts, $js_min, $optimizations);
+?>
+<?php endforeach;
+  }
+
+
+  /** Return a Closure build script for a given file array.
+  */
+  protected function _closure($file_array, $output, $comp_level = 'simple') {
+
+    $base_url = base_url(); //.'application/';
+    $rand = rand(0, 100);
+
+    $levels = array(
+      0 => 'WHITESPACE_ONLY',
+      'whitespace' => 'WHITESPACE_ONLY',
+      'simple' => 'SIMPLE_OPTIMIZATIONS',
+      'advanced' => 'ADVANCED_OPTIMIZATIONS',
+    );
+    $comp_level = $levels[$comp_level];
+    $output = basename($output);
+
+    $url_list = '';
+    foreach ($file_array as $script) {
+      $asset_url = FALSE===strpos($script, 'http') ? $base_url : '';
+	  $url_list .= "// @code_url $asset_url$script?r=$rand".PHP_EOL;
+    }
+
+    $closure = str_replace(
+      array('//__URLS__', '__OUTPUT__', '__LEVEL__'),
+      array($url_list, $output, $comp_level),
+      $this->_closure_template);
+
+    return $closure;
+  }
+
+  /** Build the theme CSS & Javascript at the commandline.
+  */
+  protected function _cli_builder() {
+
+    $this->_build($this->theme->styles, $this->theme->css_min);
+
+  }
+
+  /** Build (join) and minify a given array of files.
+  */
+  protected function _build($file_array, $output) {
+    echo "Building... $output".PHP_EOL;
+  
+    $app_path = dirname(__DIR__) .'/';
+    $temp_file = $app_path. str_replace('.min', '', $output);
+
+    $buffer = '';
+    foreach ($file_array as $script) {
+      $buffer .= file_get_contents($app_path. $script);
+    }
+    $res = file_put_contents($temp_file, $buffer);
+
+    return $this->_yui_compress($temp_file, $app_path. $output);
+  }
+
+  /** Run YUI Compressor on a CSS or Javascript file. 
+  */
+  protected function _yui_compress($input, $output) {
+    define('YUI_COMPRESSOR', APPPATH .'libraries/yuicompressor/yuicompressor-2.4.6.jar');
+
+    $cmd = "java -jar ".YUI_COMPRESSOR." $input -o $output -v --charset utf-8 ";
+    $result = system($cmd);
+    return $result;
+  } 
+}
--- application/libraries/trackers/Capret_Tracker.php
+++ application/libraries/trackers/Capret_Tracker.php
@@ -16,37 +16,51 @@
 
 /**
  * CaPReT tracker
- * A wrapper around CaPReT, with Track OER extensions.
+ * A wrapper around CaPRéT, with Track OER extensions.
  */
 class Capret_Tracker extends Base_Tracker {
 
 
-  public function getJavascripts($clipboard_2 = TRUE, $with_piwik = TRUE, $debug = FALSE) {
+  public function getJavascripts($capret_mode = NULL, $jquery_version = NULL /*'1.6.2'*/, $debug = FALSE, $clipboard_2 = FALSE) {
+    $stub = 'capret/js/';
     $scripts = array();
 
     if ($debug) {
       $scripts['[if lt IE 9]'] = 'http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js';
     }
     if ($clipboard_2) {
-      $scripts['[if IE]'] = 'ierange.js';
+      $scripts['[if IE]'] = $stub .'jsierange.js';
     }
-    $scripts['jquery'] = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js';
+    if ($jquery_version) {
+      $scripts['jquery'] = 'https://ajax.googleapis.com/ajax/libs/jquery/'. $jquery_version .'/jquery.min.js';
+    }
 
     if ($clipboard_2) {
-      $scripts[] = 'jquery.clipboard2.js';
+      $scripts[] = $stub .'jquery.clipboard2.js';
     } else {
       // Original.
-      $scripts[] = 'jquery.plugin.clipboard.js';
+      $scripts[] = $stub .'jquery.plugin.clipboard.js';
     }
 
-    $scripts[] = 'oer_license_parser.js';
+    $scripts[] = $stub .'oer_license_parser.js';
 
-    if ($with_piwik) {
-      $scripts['[if lt IE 8]'] = 'https://cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2.js';
-      $scripts['@data'] = 'capret-piwik.js';
-    } else {
-      // Original.
-      $scripts[] = 'capret.js';
+
+    switch ($capret_mode) {
+      case 'piwik':
+        $scripts['[if lt IE 8]'] = 'https://cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2.js';
+        $scripts['@data'] = $stub .'capret-piwik.js';
+      break;
+      case 'ga':
+      case 'google': //Fall-through.
+        $scripts[] = 'public/js/gajs-ext-js';
+        $scripts['@data'] = $stub .'capret-ga.js';
+      break;
+      case 'classic': // Fall-through.
+      case 'original':
+      case NULL:
+      default:  // Original.
+        $scripts[] = $stub .'capret.js';
+      break;
     }
 
     return $scripts;
@@ -56,7 +70,7 @@ class Capret_Tracker extends Base_Tracker {
   public function renderScripts($scripts, $data_attr = array()) {
 
     $view_data = array(
-      'script_url' => base_url() .'/assets/capret/js/',
+      'script_url' => base_url(), #.'/assets/capret/js/',
       'scripts' => $scripts,
       'data_attr' => $data_attr,  
     );
@@ -66,6 +80,12 @@ class Capret_Tracker extends Base_Tracker {
 
 
   public function track($service, $site_id, $image, $source_host, $source_identifier=NULL, $source_path=NULL, $title=NULL, $referer=NULL, $record = 1) {
+    return NULL;
+  }
+  public function isValid($id) {
+    return NULL;
+  }
+  public function getDefaultId() {
     return NULL;
   }
 }

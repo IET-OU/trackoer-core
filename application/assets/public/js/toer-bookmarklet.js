352e8152c9e883f053bee75a160d2528f3fa88c2
Nick Freear <nfreear@yahoo.co.uk>
2012-10-02 14:03:52 +0100

    Fix to wording for Bookmarklet view - for MSIE/Internet Explorer
    * Plus, JSLint fixes to toer-bookmarklet Javascript

--- application/assets/public/js/toer-bookmarklet.js
+++ application/assets/public/js/toer-bookmarklet.js
@@ -2,26 +2,31 @@
  Track OER bookmarklet.
  (c)2012-09-11 The Open University.
 */
-/*jslint browser: true, devel: true, indent: 4 */
+/*jslint browser:true, devel:true, indent:4 */
+/*global parent, document, window, alert */
 
 (function () {
+	'use strict';
 
 	var params = 'edge=1',
 		PWL = parent.window.location,
 		D = document,
 		//DL = d.location,
 		enc = encodeURIComponent;
-	if (typeof D.trackoer_srvurl == 'undefined') {
-		alert("Error, missing 'srvurl'!");
+	if (typeof D.trackoer_srvurl === 'undefined') {
+		alert("Error, missing 'trackoer_srvurl'!");
 	}
-	else if (D.trackoer_srvurl.indexOf(PWL.host) != -1) {
+	else if (typeof D.trackoer_errtext === 'undefined') {
+		alert("Error, missing 'trackoer_errtext'!");
+	}
+	else if (D.trackoer_srvurl.indexOf(PWL.host) !== -1) {
 		// IF "Self", do nothing..
 	}
-	else if (PWL.host.match(/(labspace.*?\.open.ac.uk|openlearn.*?\.open.ac.uk|oercommons\.org)/)) {
+	else if (PWL.host.match(/(labspace.*?\.open\.ac\.uk|openlearn\.*?\.open\.ac\.uk|oercommons\.org)/)) {
 		PWL.href = D.trackoer_srvurl + 'oerform?' + params + '&url=' + enc(PWL.href);
 	} else {
 		alert(
-			PWL. host + ' : '+ D.trackoer_errtext
+			PWL.host + ' : ' + D.trackoer_errtext
 			/*"Sorry, '"
 			+ PWL.host + 
 			"' is not supported by Track OER.\n\n The license-tracker service currently supports:\nOpenlearn.open.ac.uk, Labspace.open.ac.uk and OERCommons.org"
--- application/views/site_layout/bookmarklet.php
+++ application/views/site_layout/bookmarklet.php
@@ -3,7 +3,11 @@
 <?php
 	$base_url = base_url();
 ?>
-<span id="bookmarklet" title="Drag the bookmarklet link to your browser's favourites/ bookmarks bar"><span>Bookmarklet </span><a
+<span id="bookmarklet" title=
+"Drag the bookmarklet link to your browser's favorites/ bookmarks bar, or..
+&lt;br />
+Right-click on the bookmarklet and choose &ldquo;Add to Favorites...&rdquo;"
+><span>Bookmarklet </span><a
  title="Track OER<?php if(TRACKOER_LIVE_URL != $base_url): ?> dev<?php endif; ?>" role="button" rel="bookmark" type="application/javascript" href=
 "javascript:(function(){var d=document,s=d.createElement('script');s.type='text/javascript';s.src='<?php
   echo $base_url ?>public/js/toer-bookmarklet.js?x='+(Math.random());d.getElementsByTagName('head')[0].appendChild(s);d.trackoer_srvurl='<?php

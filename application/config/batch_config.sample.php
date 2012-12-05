1e5fe6ed66ae1608139a6368e8b7f6ecf1b69fce
Nick Freear <nfreear@yahoo.co.uk>
2012-12-05 22:19:34 +0000

    Improve 'config/batch_config.sample' - make it easier to use for B2S

--- application/config/batch_config.sample.php
+++ application/config/batch_config.sample.php
@@ -14,25 +14,26 @@
 */
 
 
-$_dir = 'C:/Users/<USER>/workspace/_trackoer/Learning_to_Learn_1.0_plain';
+$mode = 'plain';  #TRACKER_MODE_SCORM; #'scorm' 'imscc' 'plain'
+$account = 'UA-1234578-9';
+$course = 'Learning_to_Learn_1.0';
+$course_NEXT = 'Succeed_with_Math_1.0';
+$_dir = 'C:/Users/<USER>/workspace/_trackoer_b2s_zips';
+
 
 /*
 | Command line batch processing -- options will override this configuration.
 */
 $config['cli_batch'] = array(
-    'url' => 'http://labspace.open.ac.uk/Learning_to_Learn_1.0', //Course URL.
-    'ac'  => 'UA-1234578-9',
-    'mode' => 'zip',  // Was 'fmt'
+    'url' => "http://labspace.open.ac.uk/$course", //Course URL.
+    'ac'  => $account,
+    'mode' => $mode,  // Was 'fmt'
     'lic' => 'cc:by',
     #'lic' => 'cc:by-nc-sa/2.0/uk/88x31.png',
-    'dir' => "$_dir/Items",
-    'out' => "$_dir/../Learning_to_Learn_1.0_plain_wt2/Items",
-    #'dir' => '/input/directory',
-    #'out' => '"C:/output directory"',
+    'dir' => "$_dir/{$course}_$mode/Items",
+    'out' => "$_dir/{$course}_{$mode}_wt2/Items"
     'log' => '%2Flogs%2Ffile.log',  // Currently unused.
     'jspath' => '../Shared',
     'css' => 'font-size:x-small; margin:2em 0; padding:6px; background:#eee; border:1px solid #ddd;',
-
-   # 'E-test' => '"Error, unrecognised CLI argument.."',
 );
 
